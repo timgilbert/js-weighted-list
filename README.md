10f86f9e3b367d347290fb9edda1bf1c35bc6439
Tim Gilbert <timgilbert@gmail.com>
2012-03-14 16:20:42 -0400

    Initial commit (port from how-you-been)

---
+++ README.md
@@ -0,0 +1,93 @@
+js-weighted-list
+================
+
+This is a smallish library which implements a weighted list, from which elements can be picked out at random with 
+a probability dependent on their weight.  The list implements random sampling without replacement.
+
+As an example:
+
+```javascript
+    var data = [['a', 10],  
+                ['b',  1],
+                ['c',  1],
+                ['d',  5],
+                ['e',  3]];
+    
+    var wl = new WeightedList(data);
+    
+    // wl.peek() returns items at random from the list, and does not modify the list.
+    var result = wl.peek();   // Ex: ['a']
+    var result = wl.peek(3);  // Ex: ['a', 'c', 'd'] 
+    
+    // wl.pop() returns random items from the list and removes the items it found
+    var result = wl.pop(2);  // Ex: ['a', 'd'], after which wl consists of [ ['b',  1], ['c',  1], ['e',  3] ]
+    
+    // wl.push() adds new data into the set
+    // note that despite the terms push and pop, the weighted list has no natural order
+    wl.push('f', 6);     // wl is now [ ['b',  1], ['c',  1], ['e',  3], ['f',  6] ]
+    
+    // wl.addWeight() will increase the weight of a list item (or decrease it if the user passes a negative number)
+    wl.addWeight('b', 4);   // wl is now [ ['b',  5], ['c',  1], ['e',  3], ['f',  6] ]
+    
+    // wl.shuffle() will return the entire list in random order.
+    wl.shuffle();           // Ex: ['b', 'f', 'c', 'e']
+    wl.shuffle();           // Ex: ['f', 'e', 'b', 'c']
+    wl.shuffle();           // Ex: ['f', 'b', 'e', 'c']
+```
+
+This project initially grew from a javascript reimplementation of this 
+[Stack Overflow answer by Jason Orendorff][answer], and it still uses basically the same algorithm.
+
+js-weighted-list has no external dependencies and is licensed under the MIT License.
+
+Usage
+=====
+
+Include the file:
+
+```html
+    <script type="text/javascript" src="js-weighted-list.js"></script>
+```
+
+The basic object which is exported is a WeightedList, which you can instantiate like this:
+
+```javascript
+    var wl = new WeightedList();
+```
+
+Every item on the list has a key, a weight, and optionally, some other data attached.  Keys 
+must be unique.  Additional data can be passed in as a third element in the constructor list, 
+or as a third argument to the addItem() method:
+
+```javascript
+    var sandwich = new WeightedList(
+        [ ['marv', 10, { name: 'Marvin', sandwich: 'roast beef' } ],
+          ['bob',   1, { name: 'Bob', sandwich: 'turkey' } ] ]);
+   
+   // Equivalent to above
+   var sandwich = new WeightedList();
+   sandwich.push('marv', 10, { name: 'Marvin', sandwich: 'roast beef' });
+   sandwich.push('bob',   1, { name: 'Bob', sandwich: 'turkey' });
+```
+
+If a list contains data elements as described above, functions the select elements from it will return 
+objects of the form `{key: key, data: data}` instead of simply `key`.  For example:
+
+```javascript
+    sandwich.peek();     // Ex: [ {key: 'marv', data: {name: 'Marvin', sandwich: 'roast beef'}} ]
+    sandwich.shuffle();  // Ex: [ {key: 'marv', data: {name: 'Marvin', sandwich: 'roast beef'}}, 
+                         //       {key: 'bob', data: { name: 'Bob', sandwich: 'turkey' }} ]
+```
+
+
+
+TODO
+----
+
+* better error handling (stack underflow, etc)
+
+* unit tests!
+
+* jslint
+
+[answer]: http://stackoverflow.com/a/2149533/87990
---
+++ js-weighted-list.js
@@ -0,0 +1,165 @@
+/**
+ * js-weighted-list.js
+ * 
+ * This file is licensed under the MIT License, please see MIT-LICENSE.txt for details.
+ */
+function WeightedList(initial) { 
+  this.weights = {};
+  this.data = {};
+  this.length = 0;
+  this.hasData = false;
+  //console.debug(initial);
+  if (initial != null) {
+    for (var i = 0; i < initial.length; i++) {
+      var item = initial[i]
+      this.addItem(item[0], item[1], item[2]);
+    }
+  }
+}
+WeightedList.prototype = {
+  /**
+   * Add an item to the list
+   */
+  addItem: function(key, weight, data) {
+    //console.debug('k:', key, 'w:', weight, 'd:', data);
+    this.weights[key] = weight;
+    if (data != null) {
+      this.hasData = true;
+      this.data[key] = data;
+    }
+    this.length++;
+  },
+  
+  /** 
+   * Add the given weight to the list item with the given key
+   */
+  addWeight: function(key, weight) {
+    this.weights[key] += weight;
+  },
+  
+  /**
+   * Select n random elements (without replacement), default 1.
+   * If andRemove is true (default false), remove the elements
+   * from the list.  (This is what the pop() method does.)
+   */
+  peek: function(n, andRemove) {
+    if (n == null) {
+      n = 1;
+    }
+    andRemove = !!andRemove;
+    
+    heap = this._buildWeightedHeap();
+    //console.debug('heap:', heap);
+    result = [];
+    
+    for (var i = 0; i < n; i++) {
+      key = heap.pop();
+      //console.debug('k:', key);
+      if (this.hasData) {
+        result.push({key: key, value: this.data[key]});
+      } else {
+        result.push(key);
+      }
+      if (andRemove) {
+        delete this.weights[key];
+        delete this.values[key];
+        this.length--;
+      }
+    }
+    return result;
+  },
+  
+  /**
+   * Return the entire list in a random order (note that this does not mutate the list)
+   */
+  shuffle: function() {
+    return this.peek(this.length);
+  },
+  
+  /**
+   * 
+   */
+  pop: function(n) {
+    return peek(n, true);
+  },
+  
+  /**
+   * Build a WeightedHeap instance based on the data we've got
+   */
+  _buildWeightedHeap: function() {
+    var items = [];
+    for (var key in this.weights) {
+      // skip over Object.prototype monkey-patching per
+      // http://bonsaiden.github.com/JavaScript-Garden/#object.forinloop
+      if (this.weights.hasOwnProperty(key)) {
+        items.push([key, this.weights[key]]);
+      }
+    }
+    //console.log('items',items);
+    return new _WeightedHeap(items);
+  }
+}
+
+/**
+ * This is a javascript implementation of the algorithm described by 
+ * Jason Orendorff here: http://stackoverflow.com/a/2149533/87990
+ */
+function _HeapNode(weight, value, total) {
+  this.weight = weight;
+  this.value = value;
+  this.total = total;  // Total weight of this node and its children
+}
+/**
+ * Note, we're using a heap structure here for its tree properties, not as a 
+ * classic binary heap. A node heap[i] has children at heap[i<<1] and at 
+ * heap[(i<<1)+1]. Its parent is at h[i>>1]. Heap[0] is vacant.
+ */
+function _WeightedHeap(items) {
+  this.heap = [null];   // Math is easier to read if we index array from 1
+  
+  // First put everything on the heap 
+  for (var i = 0; i < items.length; i++) {
+    var weight = items[i][1];
+    var value = items[i][0];
+    this.heap.push(new _HeapNode(weight, value, weight));
+  }
+  // Now go through the heap and each node's weight to its parent
+  for (i = this.heap.length - 1; i > 1; i--) {
+    this.heap[i>>1].total += this.heap[i].total;
+  }
+  //console.debug('_Wh heap', this.heap);
+}
+
+_WeightedHeap.prototype = {
+  pop: function() {
+    // Start with a random amount of gas
+    var gas = this.heap[1].total * Math.random();
+    
+    // Start driving at the root node
+    var i = 1;  
+    
+    // While we have enough gas to keep going past i:
+    while (gas > this.heap[i].weight) {
+      gas -= this.heap[i].weight;     // Drive past i
+      i <<= 1;                        // Move to first child
+      if (gas > this.heap[i].total) {
+        gas -= this.heap[i].total     // Drive past first child and its descendants
+        i++;                          // Move on to second child
+      }
+    }
+    // Out of gas - i is our selected node.
+    var value = this.heap[i].value;
+    var selectedWeight = this.heap[i].weight;
+    
+    this.heap[i].weight = 0;          // Make sure i isn't chosen again
+    while (i > 0) {
+      // Remove the weight from its parent's total
+      this.heap[i].total -= selectedWeight
+      i >>= 1;  // Move to the next parent
+    }
+    return value;
+  }
+};
+
+//  NB: another binary heap implementation is at
+// http://eloquentjavascript.net/appendix2.html
