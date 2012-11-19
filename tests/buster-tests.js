1dfd31eb3bb53422e4beb07f20b67dd983fd3a3c
Tim Gilbert <timgilbert@gmail.com>
2012-11-19 01:42:03 -0500

    Adding buster stuff

---
+++ tests/buster-tests.js
@@ -0,0 +1,14 @@
+/* buster-tests.js - test cases for Buster.JS */
+
+buster.spec.expose();
+
+var spec = describe("Weighted Lists", function() {
+  describe('Constructor', function() {
+    it('should succeed with no arguments', function() {
+      //expect(new WeightedList()).to.be.ok;
+      assert(true);
+    });
+  });
+
+  it("should have a good constructor");
+});
